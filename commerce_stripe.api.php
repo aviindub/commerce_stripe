987e5594cbdbbbca1d90f39db355082ad38e96ae
glassdimly <jmjohn@riseup.net>
2015-06-29 17:06:05 -0400

    drupal alter hook and javascript changes to enable commerce_marketplace_stripe

--- commerce_stripe.api.php
+++ commerce_stripe.api.php
@@ -1,14 +1,15 @@
 <?php
-
 /**
+ *
  * @param $charge
  * This is the charge object which will be handed to Stripe_Charge::create().
  * You can modify it according to the docs here: https://stripe.com/docs/api/php#charges
- *
- * @param $transaction
- * This is the transaction created by commerce_payment_transaction_new which will be saved by commerce_payment_transaction_save
+ * @param $context is an array of variables, the context of the charge, including:
+ *  $context['dest'] will be handed as the second paramenter to Stripe_Charge::create()
+ *  $context['transaction'] is the transation object being created, for reference.
+ *  $context['payment_method'] is the enabled payment method, included for reference.
  *
  */
-function hook_commerce_stripe_transaction_alter($charge, $transaction) {
+function hook_commerce_stripe_transaction_alter(&$charge, &$transaction) {
 // No example.
 }
